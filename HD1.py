540fd9e1761a567e9082c62be478c5d0dd2542e6
Timmy <144027898+timmyngn37@users.noreply.github.com>
2025-05-23 01:06:39 +1000

    Update HD1.py

--- HD1.py
+++ HD1.py
@@ -30,17 +30,28 @@ data = pd.read_csv('user_list.csv', names=columns)
 data['Sentiment Score'] = data['Comment'].apply(get_sentiment_score)
 data['Sentiment'] = data['Comment'].apply(get_sentiment)
 
-# plot the data
+# subplots
+fig, axes = plt.subplots(2, 2, figsize = (16,8))
+
+# plot the percentages of users' temperaments
+personality_counts = data['Personality'].value_counts()
+axes[0,0].pie(personality_counts, labels = personality_counts.index, autopct = '%1.1f%%')
+axes[0,0].set_title('Personality Distribution')
+
+# plot the percentages of users' sentiments to the results
 sentiment_counts = data['Sentiment'].value_counts()
-fig, axes = plt.subplots(1, 2, figsize = (8,4))
+axes[0,1].pie(sentiment_counts, labels = sentiment_counts.index, autopct = '%1.1f%%')
+axes[0,1].set_title('Sentiment Distribution')
 
-axes[0].pie(sentiment_counts, labels = sentiment_counts.index, autopct = '%1.1f%%')
-axes[0].set_title('Sentiment Distribution')
+# plot the density of the sentiment score
+sns.kdeplot(data = data, x = 'Sentiment Score', fill = True, ax = axes[1,0])
+axes[1,0].set_title('Sentiment Score Distribution')
+axes[1,0].set_xlabel('Sentiment Score')
+axes[1,0].set_ylabel('Density')
 
-sns.kdeplot(data = data, x = 'Sentiment Score', fill = True, ax = axes[1])
-axes[1].set_title('Sentiment Score Distribution')
-axes[1].set_xlabel('Sentiment Score')
-axes[1].set_ylabel('Density')
+sns.scatterplot(data = data, x = 'Personality', y = 'Sentiment Score', ax = axes[1,1], hue = 'Sentiment')
+axes[1,1].set_title('Sentiment Score by Personality')
 
+fig.suptitle("The Results", fontsize=16)
 plt.tight_layout()
 plt.show()
